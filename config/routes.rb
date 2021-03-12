b66d554985bb81dad2c5e2f74eb31a2dcf00eefb
arvinegopez <gopezarvine@ymail.com>
2021-03-12 07:14:18 -0600

    added the routes to show index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'pokemons/index'
-  get 'pokemons/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/pokemons", to: "pokemons#index", as: "pokemons"
+  get "/pokemons/:id", to: "companies#show", as: "pokemon"
+
 end
